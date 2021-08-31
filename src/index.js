0f90780cd9109439a5c0fcf8e79042327d6d3d7b
wsw0108 <wsw0108@gmail.com>
2021-08-31 18:15:49 +0800

    open-file-dialog

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { dialog, ipcMain } = require('electron');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
+ipcMain.handle('show-open-dialog', async (_, options) => {
+  await dialog.showOpenDialog(
+    BrowserWindow.getFocusedWindow(),
+    options,
+  );
+})
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    // show: false,
+    // frame: false,
+    // titleBarStyle: 'hidden',
+    // backgroundColor: 'red',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false
+    }
   });
+
+  process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
